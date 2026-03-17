7b76815d45f58efb3e2d6402356cf66e6409c0f0
Malik Ahsan <malikahsan13@gmail.com>
2026-03-18 02:12:24 +0500

    adding more assertions

--- test_main.py
+++ test_main.py
@@ -1,4 +1,12 @@
-from main import get_weather
+# from main import get_weather
 
-def test_get_weather():
-    assert(get_weather(21) == "cold")
+# def test_get_weather():
+#     assert(get_weather(21) == "cold")
+
+from main import add, divide
+import pytest
+
+def test_add():
+    assert add(2, 3) == 5 "2 + 3 should be 5"
+    assert add(-1 , 1) == 5 "2 + 3 should be 5"
+    assert add(2, 3) == 5 "2 + 3 should be 5"
