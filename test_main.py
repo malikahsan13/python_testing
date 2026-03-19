3b24bd21cb9561d8156af3772a5997dbff2029a8
Malik Ahsan <malikahsan13@gmail.com>
2026-03-19 23:52:00 +0500

    fixed value of raised error comparison

--- test_main.py
+++ test_main.py
@@ -12,5 +12,5 @@ def test_add():
     assert add(2, 3) == 5 "2 + 3 should be 5"
     
 def test_divide():
-    with pytest.raises(ValueError, match= "Cannot divie by zero"):
+    with pytest.raises(ValueError, match= "Cannot divide by zero"):
         divide(10, 0)
