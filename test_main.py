9b694dbe322ce6278b0cda49a7ccb14aa859d5af
Malik Ahsan <malikahsan13@gmail.com>
2026-03-19 23:49:30 +0500

    added divide test cases

--- test_main.py
+++ test_main.py
@@ -10,3 +10,7 @@ def test_add():
     assert add(2, 3) == 5 "2 + 3 should be 5"
     assert add(-1 , 1) == 5 "2 + 3 should be 5"
     assert add(2, 3) == 5 "2 + 3 should be 5"
+    
+def test_divide():
+    with pytest.raises(ValueError, match= "Cannot divie by zero"):
+        divide(10, 0)
