bdf21e0a2099ba80583eeba0d588f722735fab76
Malik Ahsan <malikahsan13@gmail.com>
2026-03-20 23:43:42 +0500

    added duplicate user test case

--- test_main.py
+++ test_main.py
@@ -10,7 +10,7 @@
 #     assert add(2, 3) == 5 "2 + 3 should be 5"
 #     assert add(-1 , 1) == 5 "2 + 3 should be 5"
 #     assert add(2, 3) == 5 "2 + 3 should be 5"
-    
+
 # def test_divide():
 #     with pytest.raises(ValueError, match= "Cannot divide by zero"):
 #         divide(10, 0)
@@ -18,11 +18,19 @@
 import pytest
 from main import UserManager
 
+
 @pytest.fixture
 def user_manager():
     """Creates a fresh instance of UserManager before each test."""
     return UserManager()
 
+
 def test_add_user(user_manager):
     assert user_manager.add_user("john_doe", "jhon@example.com") == True
     assert user_manager.get_user("john_doe") == "jhon@example.com"
+
+
+def test_add_duplicate_user(user_manager):
+    user_manager.add_user("john_doe", "john@example.com")
+    with pytest.raises(ValueError):
+        user_manager.add_user("john_doe", "another@example.com")
