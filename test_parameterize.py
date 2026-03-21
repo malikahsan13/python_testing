e4d3d736befc64835736b23a411db426c206686a
Malik Ahsan <malikahsan13@gmail.com>
2026-03-22 01:20:18 +0500

    added some more values for testing

--- test_parameterize.py
+++ test_parameterize.py
@@ -8,6 +8,9 @@ from parameterize import is_prime
     (3, True),
     (4, False),
     (17, True),
+    (18, False),
+    (19, True),
+    (25, False)
 ])
 def test_is_prim(num, expected):
     assert is_prime(num) == expected
